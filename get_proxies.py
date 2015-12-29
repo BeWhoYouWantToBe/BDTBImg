479c7c08a83c1df8fc7787c13ae212b95f280b51
king <king@localhost.localdomain>
2015-12-29 10:54:58 +0800

    多线程 依然会被反爬虫

---
+++ get_proxies.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pdb
+import requests 
+from queue import Queue
+from threading import Thread
+from bs4 import BeautifulSoup 
+
+class proxy():
+    def __init__(self):
+        self.urls = ['http://www.kuaidaili.com/proxylist/','http://www.kxdaili.com/ipList/{}.html#ip']
+        self.dir = '/home/king/Documents/'
+        self.proxies_list = []
+        self.headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+
+    def get_proxies(self):
+        proxies = set() 
+        for url in self.urls:
+            if url == self.urls[0] or url == self.urls[1]:
+                for i in range(1,11):
+                    if url == self.urls[0]:
+                        purl = url + str(i)
+                    else:
+                        purl = url.format(i)
+                    r = requests.get(purl,headers=self.headers)
+                    soup = BeautifulSoup(r.text,'lxml')
+                    try:
+                        trs = soup.find_all('tr')[1:] 
+                    except:
+                        print('抓取代理失败')
+                    else:
+                        for tr in trs:
+                            proxy = tr.contents[1].string + ':' +  tr.contents[3].string
+                            proxies.add(proxy)
+        return proxies 
+
+    def judge_proxies(self,proxy):
+            proxies = {'http':'http://'+proxy}
+            try:
+                r = requests.get('http://www.meizitu.com',proxies=proxies,headers=self.headers,timeout=8)
+            except requests.exceptions.ConnectTimeout: 
+                print(proxy+'Timeout')
+            except :
+                print('ERROR')
+            else:
+                self.proxies_list.append(proxy)
+                print(proxy+' OK')
+
+    def save_proxies(self):
+        path = self.dir + 'proxies' 
+        f = open(path,'w')
+        for proxy in self.proxies_list:
+            f.write(proxy+'\n') 
+        f.close()
+
+def main():
+    q = Queue()
+    NUM = 30
+    proxies = proxy() 
+    jobs = proxies.get_proxies()
+    def mult_thread():
+        while True:
+            proxy = q.get()
+            proxies.judge_proxies(proxy)
+            q.task_done()
+    for i in range(NUM):
+        t = Thread(target=mult_thread)
+        t.setDaemon(True) 
+        t.start() 
+    for p in jobs:
+        q.put(p)
+    q.join()
+    if __name__=='__main__':
+        proxies.save_proxies()
+    return proxies.proxies_list
+
+
+if __name__=='__main__':
+    main()
+
+        
+
+
--- meizitu.py
+++ meizitu.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python
 # coding=utf-8
-import os 
+import os
 import pdb
 import time
-import gevent 
 import requests
-from bs4 import BeautifulSoup 
+import random
+import get_proxies
+from bs4 import BeautifulSoup
+from threading import Thread
+from queue import Queue
 
-headers = {'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'}
+
+
+def get_proxy():
+    return get_proxies.main()
+
+all_proxies = get_proxy()
 
 def get_urls():
     urls = {}
-    for i in range(1,3):
+    for i in range(1, 3):
         tag = 1
-        time.sleep(5)
+        time.sleep(6)
         url = 'http://www.meizitu.com/a/list_1_{}.html'.format(i)
         while tag:
             wait = 10
             try:
-                r = requests.get(url,timeout=5,headers=headers)
+                r = requests.get(url, timeout=8, headers=headers)
                 tag = 0
             except:
-                time.sleep(wait) 
+                time.sleep(wait)
                 wait += 5
         r.encoding = 'gb2312'
-        soup = BeautifulSoup(r.text,'lxml') 
-        div_tags = soup.find_all('div',{'class':'pic'})
+        soup = BeautifulSoup(r.text, 'lxml')
+        div_tags = soup.find_all('div', {'class': 'pic'})
         for div in div_tags:
             urls[div.img['alt'].lstrip('<b>').rstrip('</b>')] = div.a['href']
         print('第{}页完成'.format(i))
 
-    return urls 
+    return urls
+
 
 def mkdir(dirs):
     path = '/home/king/Pictures/meizitu/'
@@ -38,46 +49,74 @@ def mkdir(dirs):
         dir = path + d + '/'
         if not os.path.exists(dir):
             os.makedirs(dir)
-    
-def get_imgs(dir,url): 
-    dir = '/home/king/Pictures/meizitu/' + dir +'/'
-    r = requests.get(url,timeout=5,headers=headers) 
+
+
+def get_imgs(dir,url):
+    dir = '/home/king/Pictures/meizitu/' + dir + '/'
+    proxies = {}
+    proxies['http'] = 'http://' + random.choice(all_proxies) 
+    r = requests.get(url, timeout=8, headers=headers, proxies=proxies)
     r.encoding = 'gb2312'
-    soup = BeautifulSoup(r.text,'lxml') 
-    imgs = soup.find('div',{'id':'picture'}).find_all('img')
+    soup = BeautifulSoup(r.text, 'lxml')
+    imgs = soup.find('div', {'id': 'picture'}).find_all('img')
     for i in imgs:
         path = dir + i['alt']
-        save_img(path,i['src'])
-        print(i['alt']+' 保存完成')
+        save_img(path, i['src'])
+        print(i['alt'] + ' 保存完成')
+    time.sleep(6)
+
 
 def save_img(full_path,pic_url):
-    r = requests.get(pic_url,timeout=10,headers=headers)
-    f = open(full_path,'wb') 
+    time.sleep(6)
+    proxies = {}
+    tag = 1
+    while tag:
+        try:
+            r = requests.get(
+                pic_url,
+                timeout=10,
+                headers=headers,
+                proxies=proxies)
+            tag = 0
+        except:
+            proxies = random.choice(all_proxies)
+    f = open(full_path, 'wb')
     f.write(r.content)
     f.close()
-    
+
 
 def main():
+    q = Queue()
     urls = get_urls()
     mkdir(urls.keys())
-    for dir in urls:
-        get_imgs(dir,urls[dir])
+    def run():
+        while 1:
+            dir = q.get()
+            get_imgs(dir,urls[dir])
+            q.task_done()
+    for i in range(10):
+        t = Thread(target=run)
+        t.setDaemon(True)
+        t.start()
+    for d in urls:
+        q.put(d)
+    q.join()
+
+
 #    tasks = []
 #    while urls:
 #        dirs = []
 #        i = 0
-#        for dir in urls: 
+#        for dir in urls:
 #            if i<10:
 #                tasks.append(gevent.spawn(get_imgs,dir,urls[dir]))
 #                dirs.append(dir)
-#                i += 1 
+#                i += 1
 #            else:
 #                break
 #        for dir in dirs:
 #            del urls[dir]
-#        gevent.joinall(tasks) 
+#        gevent.joinall(tasks)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
